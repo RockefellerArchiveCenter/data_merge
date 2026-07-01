c41104b75d8c7eb81f0dcdae5b41bc359914dae0
Hillel Arnold <hillel.arnold@gmail.com>
2026-07-01 10:40:25 -0400

    improve logging

--- src/merge_data.py
+++ src/merge_data.py
@@ -4,12 +4,14 @@ import traceback
 from os import getenv
 
 import boto3
+from requests.exceptions import ConnectionError
+from urllib3.exceptions import ConnectTimeoutError, ProtocolError
 
 from .mergers import (AgentMerger, ArchivalObjectMerger, ArrangementMapMerger,
                       ResourceMerger, SubjectMerger)
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(getenv('LOGGING_LEVEL', logging.INFO))
 
 SERVICE_NAME = 'data_merge'
 FULL_CONFIG_PATH = f"/{getenv('ENV')}/{getenv('APP_CONFIG_PATH')}"
@@ -50,7 +52,7 @@ def get_config(ssm_parameter_path):
             section_name = param_path_array[section_position]
             configuration[section_name] = param.get('Value')
     except BaseException:
-        print("Encountered an error loading config from SSM.")
+        logging.error("Encountered an error loading config from SSM.")
         traceback.print_exc()
     finally:
         return configuration
@@ -124,16 +126,21 @@ def lambda_handler(event, context):
 
     config = get_config(FULL_CONFIG_PATH)
     for record in event['Records']:
-        object_data = json.loads(record['body'])
-        attributes = record['messageAttributes']
-        object_type = attributes['object_type']['stringValue']
-        source_service = attributes['service']['stringValue']
-        session_token_key = attributes['session_token_key']['stringValue']
-        config['AS_SESSION_TOKEN'] = get_session_token(source_service, session_token_key)
         try:
+            object_data = json.loads(record['body'])
+            attributes = record['messageAttributes']
+            object_type = attributes['object_type']['stringValue']
+            source_service = attributes['service']['stringValue']
+            session_token_key = attributes['session_token_key']['stringValue']
+            config['AS_SESSION_TOKEN'] = get_session_token(source_service, session_token_key)
             merger = MERGER_MAP[object_type]
+            logging.debug(f"Starting merge with {object_data}")
             data, target_object_type = merger(config).merge(object_data)
             send_success_message(config, data, target_object_type)
+            logging.debug(f"{object_type} object {data['uri']} merged")
+        except (ConnectionError, ConnectTimeoutError, ProtocolError) as e:
+            logging.error(e)
+            raise Exception("Connection error, instance will be retried.")
         except Exception as e:
-            print(e)
+            logging.error(e)
             send_failure_message(config, object_data, object_type, e)
